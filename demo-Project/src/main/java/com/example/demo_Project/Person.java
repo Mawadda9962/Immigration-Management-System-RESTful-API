f895179bc9cec5fd00171161bc364d32fb5d8e26
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-15 09:35:31 +0400

    Adding private fields

--- demo-Project/src/main/java/com/example/demo_Project/Person.java
+++ demo-Project/src/main/java/com/example/demo_Project/Person.java
@@ -1,7 +1,13 @@
 package com.example.demo_Project;
 
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 @MappedSuperclass
 public class Person {
     private String firstName;
