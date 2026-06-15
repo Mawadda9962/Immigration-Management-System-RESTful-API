77ed06879d66b462b8bed6d0b098709bb9433cd6
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-15 11:00:39 +0400

    Create Spring Data JPA repositories

--- demo-Project/src/main/java/com/example/demo_Project/Repositories/ApplicantRepository.java
+++ demo-Project/src/main/java/com/example/demo_Project/Repositories/ApplicantRepository.java
@@ -4,7 +4,9 @@ import com.example.demo_Project.Entities.Applicant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ApplicantRepository extends JpaRepository<Applicant,Long> {
-
+    List<Applicant> findByNationality(String nationality);
 }
--- demo-Project/src/main/java/com/example/demo_Project/Repositories/InterviewRepository.java
+++ demo-Project/src/main/java/com/example/demo_Project/Repositories/InterviewRepository.java
@@ -5,6 +5,9 @@ import com.example.demo_Project.Entities.Interview;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InterviewRepository extends JpaRepository<Interview,Long> {
+    List<Interview> findByOfficerIdAndInterviewDate(Long officerId, String date);
 }
