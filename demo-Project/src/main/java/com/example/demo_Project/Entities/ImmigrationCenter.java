601709ae4d3be0b0b46a5d11c6678e82f5d1341f
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-15 10:30:39 +0400

    Creating VisaApplication Entites

--- demo-Project/src/main/java/com/example/demo_Project/Entities/ImmigrationCenter.java
+++ demo-Project/src/main/java/com/example/demo_Project/Entities/ImmigrationCenter.java
@@ -1,9 +1,27 @@
 package com.example.demo_Project.Entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
 
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 @Entity
 public class ImmigrationCenter {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String locationCountry;
+    private String type;
+    private int dailyCapacity;
+
+    @OneToMany
+    private List<Officer> officers;
 
 }
--- demo-Project/src/main/java/com/example/demo_Project/Entities/ImmigrationOfficer.java
+++ demo-Project/src/main/java/com/example/demo_Project/Entities/ImmigrationOfficer.java
@@ -1,10 +1,15 @@
 package com.example.demo_Project.Entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 @Entity
 public class ImmigrationOfficer extends Person{
 
