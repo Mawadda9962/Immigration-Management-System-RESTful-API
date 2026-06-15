b73b7906cd66e3becaf8cade6c0985d2772b4622
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-15 15:02:51 +0400

    Global Exception Handling

--- demo-Project/src/main/java/com/example/demo_Project/Service/ApplicantService.java
+++ demo-Project/src/main/java/com/example/demo_Project/Service/ApplicantService.java
@@ -17,20 +17,27 @@ public class ApplicantService {
     @Autowired
     InterviewRepository interviewRepository;
 
-
-public Applicant saveApplicant(Applicant applicant) {
-    if (applicant.getPassportNumber() == null || applicant.getPassportNumber().isEmpty()) {
-        throw new SystemExceptions("Error:Passport number is required");
+    //Save full Applicant Object
+    public Applicant saveApplicant(Applicant applicant) {
+        if (applicant.getPassportNumber() == null || applicant.getPassportNumber().isEmpty()) {
+            throw new SystemExceptions("Error:Passport number is required");
+        }
+        if (applicant.getFirstName() == null || applicant.getFirstName().isEmpty()) {
+            throw new SystemExceptions("Error:first name is required");
+        }
+        if (applicant.getLastName() == null || applicant.getLastName().isEmpty()) {
+            throw new SystemExceptions("Error:Last name is required");
+        }
+        return applicantRepository.save(applicant);
     }
-    if (applicant.getFirstName() == null || applicant.getFirstName().isEmpty()) {
-        throw new SystemExceptions("Error:first name is required");
-    }
-    if (applicant.getLastName() == null || applicant.getLastName().isEmpty()) {
-        throw new SystemExceptions("Error:Last name is required");
-    }
-    return applicantRepository.save(applicant);
-}
 
+    //Save From individual Strings
+    public Applicant saveApplicant(String firstName, String lastName, String passportNumber, String nationality) {
 
+        if (passportNumber == null || passportNumber.isEmpty()){
+            throw
+        }
+
+    }
 
 }
