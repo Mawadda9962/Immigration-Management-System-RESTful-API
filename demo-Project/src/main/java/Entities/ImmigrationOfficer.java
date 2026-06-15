1747f96aca492c4c0b696a84763ef8ac59d8683b
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-15 10:02:59 +0400

    Defining the primery Key of the ImmigrationOfficer class

--- demo-Project/src/main/java/Entities/ImmigrationOfficer.java
+++ demo-Project/src/main/java/Entities/ImmigrationOfficer.java
@@ -1,6 +1,17 @@
 package Entities;
 
-@E\
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class ImmigrationOfficer extends Person{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
 
 }
