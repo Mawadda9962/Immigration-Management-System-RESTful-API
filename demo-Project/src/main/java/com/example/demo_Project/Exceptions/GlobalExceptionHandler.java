135020ee51fe0b13ae88f8e70b8eeddab46921f9
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-15 14:57:57 +0400

    Global Exception Handling

--- demo-Project/src/main/java/com/example/demo_Project/Exceptions/GlobalExceptionHandler.java
+++ demo-Project/src/main/java/com/example/demo_Project/Exceptions/GlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.context.request.WebRequest;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    //H
+    //Handles Immigration System Exceptions (404 - Resource Not Found)
     @ExceptionHandler(SystemExceptions.class)
     public ResponseEntity<ErrorResponse> handleSystemExceptions(SystemExceptions ex, WebRequest request){
         ErrorResponse errorResponse = new ErrorResponse(
@@ -23,5 +23,17 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
+    //Handle Any Unexpected System Crashes
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Internal Server Error",
+                "An unexpected error occurred on the server.",
+                request.getDescription(false).replace("uri=", "")
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
 
-}
--- demo-Project/src/main/java/com/example/demo_Project/Service/ApplicantService.java
+++ demo-Project/src/main/java/com/example/demo_Project/Service/ApplicantService.java
@@ -2,6 +2,7 @@ package com.example.demo_Project.Service;
 
 
 import com.example.demo_Project.Entities.Applicant;
+import com.example.demo_Project.Exceptions.SystemExceptions;
 import com.example.demo_Project.Repositories.ApplicantRepository;
 import com.example.demo_Project.Repositories.InterviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +17,20 @@ public class ApplicantService {
     @Autowired
     InterviewRepository interviewRepository;
 
+
 public Applicant saveApplicant(Applicant applicant) {
     if (applicant.getPassportNumber() == null || applicant.getPassportNumber().isEmpty()) {
-        throw new RuntimeException("Error:Passport number is required");
+        throw new SystemExceptions("Error:Passport number is required");
     }
     if (applicant.getFirstName() == null || applicant.getFirstName().isEmpty()) {
-        throw new RuntimeException("Error:first name is required");
+        throw new SystemExceptions("Error:first name is required");
     }
     if (applicant.getLastName() == null || applicant.getLastName().isEmpty()) {
-        throw new RuntimeException("Error:Last name is required");
+        throw new SystemExceptions("Error:Last name is required");
     }
     return applicantRepository.save(applicant);
 }
+
 
 
 }
