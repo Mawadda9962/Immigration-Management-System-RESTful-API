1f6784dc2605de6bf06750bc31faa303e9b4190f
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-15 12:06:41 +0400

    Create Spring Data JPA repositories

--- demo-Project/src/main/java/com/example/demo_Project/Repositories/OfficerRepository.java
+++ demo-Project/src/main/java/com/example/demo_Project/Repositories/OfficerRepository.java
@@ -4,6 +4,9 @@ import com.example.demo_Project.Entities.ImmigrationOfficer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OfficerRepository extends JpaRepository<ImmigrationOfficer, Long> {
+    List
 }
