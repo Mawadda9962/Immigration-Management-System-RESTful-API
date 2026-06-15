4a67b0911477f991529b5afc1b57873163e7debf
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-15 14:49:37 +0400

    Global Exception Handling

--- demo-Project/src/main/java/com/example/demo_Project/Exceptions/GlobalExceptionHandler.java
+++ demo-Project/src/main/java/com/example/demo_Project/Exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,27 @@
 package com.example.demo_Project.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    //H
+    @ExceptionHandler(SystemExceptions.class)
+    public ResponseEntity<ErrorResponse> handleSystemExceptions(SystemExceptions ex, WebRequest request){
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND,
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getDescription(false).replace("uri=","")
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+
 }
---
+++ demo-Project/src/main/java/com/example/demo_Project/Exceptions/SystemExceptions.java
@@ -0,0 +1,7 @@
+package com.example.demo_Project.Exceptions;
+
+public class SystemExceptions extends RuntimeException {
+    public SystemExceptions(String message) {
+        super(message);
+    }
+}
