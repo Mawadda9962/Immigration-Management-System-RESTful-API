e6adcf359fea1ef06d76665b7088dfeb9e7a9574
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-15 09:46:31 +0400

    Creating extends class

--- demo-Project/src/main/java/Entities/Application.java
+++ demo-Project/src/main/java/Entities/Application.java
@@ -1,11 +1,17 @@
 package Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Application extends Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String
 
 
 
