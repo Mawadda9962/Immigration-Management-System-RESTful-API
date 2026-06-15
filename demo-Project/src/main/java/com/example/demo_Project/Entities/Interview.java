094f4b9712ded71a821522bc9cdc5a7c9e219e6e
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-15 10:39:54 +0400

    Creating VisaApplication Entites

--- demo-Project/src/main/java/com/example/demo_Project/Entities/Interview.java
+++ demo-Project/src/main/java/com/example/demo_Project/Entities/Interview.java
@@ -1,4 +1,27 @@
 package com.example.demo_Project.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Entity
 public class Interview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Applicant applicant;
+
+    @ManyToOne
+    private ImmigrationOfficer officer;
+
+    private String interviewDate;
+    private String status;
+    private String purpose;
 }
