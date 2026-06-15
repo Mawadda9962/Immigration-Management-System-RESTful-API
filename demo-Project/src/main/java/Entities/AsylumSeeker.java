3ec339b66072b072d8f18a4593324c86ca41cdcb
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-15 10:01:04 +0400

    Creating Officer Entities

--- demo-Project/src/main/java/Entities/AsylumSeeker.java
+++ demo-Project/src/main/java/Entities/AsylumSeeker.java
@@ -1,10 +1,15 @@
 package Entities;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 @Entity
 public class AsylumSeeker extends Person {
     private String countryOfOrigin;
     private String sponsorOrganization;
-
 }
