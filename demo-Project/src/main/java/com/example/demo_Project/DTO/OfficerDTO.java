f67af42651484da55897326795c0004bc43d298a
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 10:58:45 +0400

    Adding the privet fields on the OfficerDTO

--- demo-Project/src/main/java/com/example/demo_Project/DTO/OfficerDTO.java
+++ demo-Project/src/main/java/com/example/demo_Project/DTO/OfficerDTO.java
@@ -1,5 +1,14 @@
 package com.example.demo_Project.DTO;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class OfficerDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private
 
 }
