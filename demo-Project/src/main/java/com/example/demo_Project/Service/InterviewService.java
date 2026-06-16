1544364542819bb0348208a1862d756ff40b0524
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 09:47:26 +0400

    Implemented processVisa

--- demo-Project/src/main/java/com/example/demo_Project/Service/InterviewService.java
+++ demo-Project/src/main/java/com/example/demo_Project/Service/InterviewService.java
@@ -1,7 +1,14 @@
 package com.example.demo_Project.Service;
 
+import com.example.demo_Project.Repositories.InterviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InterviewService {
+
+    @Autowired
+    InterviewRepository interviewRepository;
+
+
 }
