cef833e0cee6202932b912f22937384d19ec831d
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 10:48:16 +0400

    Adding the Constructor, Getter, and Setter

--- demo-Project/src/main/java/com/example/demo_Project/DTO/ApplicantDTO.java
+++ demo-Project/src/main/java/com/example/demo_Project/DTO/ApplicantDTO.java
@@ -1,13 +1,19 @@
 package com.example.demo_Project.DTO;
 
+import lombok.Data;
 import org.springframework.core.SpringVersion;
 
+@Data
 public class ApplicantDTO {
     private Long id;
     private String firstName;
     private String lastName;
     private String nationality;
     private String passportNumber;
+    private boolean criminalRecorde;
+
+
+
 
 
 
