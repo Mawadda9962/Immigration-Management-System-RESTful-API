44bba48c33f11d3666ba6631b31dd259da07dbdc
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 08:55:01 +0400

    Implemented promoteOfficer method

--- demo-Project/src/main/java/com/example/demo_Project/Service/OfficerService.java
+++ demo-Project/src/main/java/com/example/demo_Project/Service/OfficerService.java
@@ -1,6 +1,7 @@
 package com.example.demo_Project.Service;
 
 import com.example.demo_Project.Entities.ImmigrationOfficer;
+import com.example.demo_Project.Exceptions.Exceptions;
 import com.example.demo_Project.Repositories.CenterRepository;
 import com.example.demo_Project.Repositories.InterviewRepository;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,11 @@ public class OfficerService {
     @Override
     CenterRepository centerRepository;
 
-    public ImmigrationOfficer promoteOfficer(Long officerId, String newRank, int newClearanceLevel1){
+    public ImmigrationOfficer promoteOfficer(Long officerId, String newOfficerRank, int newClearanceLevel1){
+        if (newClearanceLevel1 < 1 || newClearanceLevel1 > 5){
+            throw Exceptions.badRequest("Clearance Level must be between 1 and 5");
 
+        }
     }
 
 
