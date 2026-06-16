84b23cedbcc959fbcc13a7564596d11a72feedeb
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 11:37:58 +0400

    Creating CenterService class

--- demo-Project/src/main/java/com/example/demo_Project/Service/CenterService.java
+++ demo-Project/src/main/java/com/example/demo_Project/Service/CenterService.java
@@ -1,4 +1,20 @@
 package com.example.demo_Project.Service;
 
+import com.example.demo_Project.Entities.ImmigrationCenter;
+import com.example.demo_Project.Repositories.CenterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CenterService {
+
+    @Autowired
+    CenterRepository centerRepository;
+
+
+    public ImmigrationCenter createCenter(ImmigrationCenter center){
+
+    }
+
+
 }
