f1967724b83485db28821174f5cdbf6a60bf9321
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 09:27:19 +0400

    Implemented submitApplication

--- demo-Project/src/main/java/com/example/demo_Project/Service/VisaApplicationService.java
+++ demo-Project/src/main/java/com/example/demo_Project/Service/VisaApplicationService.java
@@ -1,6 +1,8 @@
 package com.example.demo_Project.Service;
 
+import com.example.demo_Project.Entities.Applicant;
 import com.example.demo_Project.Entities.VisaApplication;
+import com.example.demo_Project.Exceptions.Exceptions;
 import com.example.demo_Project.Repositories.ApplicantRepository;
 import com.example.demo_Project.Repositories.OfficerRepository;
 import com.example.demo_Project.Repositories.VisaApplicationRepository;
@@ -20,6 +22,9 @@ public class VisaApplicationService {
     OfficerRepository officerRepository;
 
     public VisaApplication submitApplication(Long applicantId, String visaType){
+        Applicant applicant = applicantRepository.findById(applicantId)
+                .orElseThrow(() -> Exceptions.notFound("Applicant not found with id: " + applicantId));
+
 
     }
 
