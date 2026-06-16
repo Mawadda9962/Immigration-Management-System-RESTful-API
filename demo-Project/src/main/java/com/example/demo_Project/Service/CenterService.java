2980f18927deac4ce04a033cb89937fa93d9efe4
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 11:41:01 +0400

    Creating CenterService class

--- demo-Project/src/main/java/com/example/demo_Project/Service/CenterService.java
+++ demo-Project/src/main/java/com/example/demo_Project/Service/CenterService.java
@@ -1,6 +1,7 @@
 package com.example.demo_Project.Service;
 
 import com.example.demo_Project.Entities.ImmigrationCenter;
+import com.example.demo_Project.Exceptions.Exceptions;
 import com.example.demo_Project.Repositories.CenterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,18 @@ public class CenterService {
 
 
     public ImmigrationCenter createCenter(ImmigrationCenter center){
+        if (center.getName() == null || center.getName().isBlank()){
+            throw Exceptions.badRequest("Center name is required");
+        }
+        if (center.getLocationCountry() == null || center.getLocationCountry().isBlank()){
+            throw Exceptions.badRequest("Location country is required");
+        }
+        if (center.getType() ==null || center.getType().isBlank()){
+            throw Exceptions.badRequest("Center Type is ")
+        }
+
+
+
 
     }
 
