67cec3c1b5b1ed93fdfff0e5022684c849b0d3a3
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 14:47:59 +0400

    Implement RestAPI endpoints

--- demo-Project/src/main/java/com/example/demo_Project/Controllers/OfficerController.java
+++ demo-Project/src/main/java/com/example/demo_Project/Controllers/OfficerController.java
@@ -1,4 +1,10 @@
 package com.example.demo_Project.Controllers;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("officers")
 public class OfficerController {
 }
