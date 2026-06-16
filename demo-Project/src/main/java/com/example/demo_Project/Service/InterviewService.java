bdacc13af6d83a0be98674f776e24f36c4b585b2
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 09:53:06 +0400

    Implemented scheduleInterview method

--- demo-Project/src/main/java/com/example/demo_Project/Service/InterviewService.java
+++ demo-Project/src/main/java/com/example/demo_Project/Service/InterviewService.java
@@ -1,11 +1,16 @@
 package com.example.demo_Project.Service;
 
+import com.example.demo_Project.Entities.Applicant;
+import com.example.demo_Project.Entities.ImmigrationOfficer;
 import com.example.demo_Project.Entities.Interview;
+import com.example.demo_Project.Exceptions.Exceptions;
 import com.example.demo_Project.Repositories.ApplicantRepository;
 import com.example.demo_Project.Repositories.InterviewRepository;
 import com.example.demo_Project.Repositories.OfficerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class InterviewService {
@@ -19,7 +24,17 @@ public class InterviewService {
     @Autowired
     OfficerRepository officerRepository;
 
-    public Interview scheduleInterview()
+    public Interview scheduleInterview(Long applicantId, Long officerId, String date){
+        Applicant applicant = applicantRepository.findById(applicantId)
+                .orElseThrow(() -> Exceptions.notFound("Applicant not found with id: " + applicantId));
+
+        ImmigrationOfficer officer = officerRepository.findById(officerId)
+                .orElseThrow(() -> Exceptions.notFound("Officer not found with id: " + officerId));
+
+
+        List<Interview> existing
+
+    }
 
 
 }
