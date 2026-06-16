1d513e027b9ed1f8e94cd5e59d70e56fbe2fc1f8
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 08:48:40 +0400

    Implemented promoteOfficer method

--- demo-Project/src/main/java/com/example/demo_Project/Service/OfficerService.java
+++ demo-Project/src/main/java/com/example/demo_Project/Service/OfficerService.java
@@ -1,4 +1,20 @@
 package com.example.demo_Project.Service;
 
+import com.example.demo_Project.Entities.ImmigrationOfficer;
+import com.example.demo_Project.Repositories.InterviewRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OfficerService {
+
+    @Override
+    OfficerService officerService;
+
+    @Override
+    InterviewRepository interviewRepository;
+
+
+
+
+
 }
