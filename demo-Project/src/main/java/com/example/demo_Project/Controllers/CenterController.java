770ef2b5a11ae62ae3e8d1993149f7078abf6388
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 14:01:25 +0400

    Creating CenterService class

--- demo-Project/src/main/java/com/example/demo_Project/Controllers/CenterController.java
+++ demo-Project/src/main/java/com/example/demo_Project/Controllers/CenterController.java
@@ -1,4 +1,24 @@
 package com.example.demo_Project.Controllers;
 
+import com.example.demo_Project.DTO.CenterDTO;
+import com.example.demo_Project.Service.CenterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("centers")
 public class CenterController {
+
+    @Autowired
+    CenterService centerService;
+
+
+    @PostMapping
+    public CenterDTO addCenter(@RequestBody)
+
+
+
 }
