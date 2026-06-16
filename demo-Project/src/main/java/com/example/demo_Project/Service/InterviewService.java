83cf377b6f01334da0759f9393a9ffd5d541020c
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 10:04:57 +0400

    Implemented completeInterView method

--- demo-Project/src/main/java/com/example/demo_Project/Service/InterviewService.java
+++ demo-Project/src/main/java/com/example/demo_Project/Service/InterviewService.java
@@ -24,7 +24,7 @@ public class InterviewService {
     @Autowired
     OfficerRepository officerRepository;
 
-    public Interview scheduleInterview(Long applicantId, Long officerId, String date){
+    public Interview scheduleInterview(Long applicantId, Long officerId, String date) {
         Applicant applicant = applicantRepository.findById(applicantId)
                 .orElseThrow(() -> Exceptions.notFound("Applicant not found with id: " + applicantId));
 
@@ -32,10 +32,10 @@ public class InterviewService {
                 .orElseThrow(() -> Exceptions.notFound("Officer not found with id: " + officerId));
 
 
-        List<Interview> existing = interviewRepository.findByOfficerIdAndInterviewDate(officerId,date){
-            if (!existing.isEmpty()){
-                throw Exceptions.badRequest("Officer is booked!");
-            }
+        List<Interview> existing = interviewRepository.findByOfficerIdAndInterviewDate(officerId, date);
+        if (!existing.isEmpty()) {
+            throw Exceptions.badRequest("Officer is double-booked!");
+        }
 
             Interview interview = new Interview();
             interview.setApplicant(applicant);
@@ -45,10 +45,12 @@ public class InterviewService {
 
             return interviewRepository.save(interview);
         }
+
+    public Interview completeInterview(Long interviewId) {
+        Interview interview = interviewRepository.findById(interviewId)
+                .orElseThrow(() -> Exceptions.notFound("Interview not found with id: " + interviewId));
+
+        interview.setStatus("COMPLETED");
+        return interviewRepository.save(interview);
     }
-
-    public Interview cancelInterview(Long interviewId) {
-
-
-
-    }
+}
