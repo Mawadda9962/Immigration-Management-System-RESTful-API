80fb1bca4873d9f4adf38d3d1539bb0462e25628
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 14:09:03 +0400

    Implement RestAPI endpoints

--- demo-Project/src/main/java/com/example/demo_Project/Controllers/ApplicantController.java
+++ demo-Project/src/main/java/com/example/demo_Project/Controllers/ApplicantController.java
@@ -1,9 +1,26 @@
 package com.example.demo_Project.Controllers;
 
+import com.example.demo_Project.DTO.ApplicantDTO;
+import com.example.demo_Project.Entities.Applicant;
+import com.example.demo_Project.Service.ApplicantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("applicant")
 public class ApplicantController {
+
+    @Autowired
+    ApplicantService applicantService;
+
+   // @Autowired
+
+    public ApplicantDTO registerApplicant(@RequestBody Applicant applicant){
+
+    }
+
+
+
 }
