867239cea447afb39613c0a8b7fcf6d7412cfecb
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 09:25:08 +0400

    Implemented submitApplication

--- demo-Project/src/main/java/com/example/demo_Project/Service/VisaApplicationService.java
+++ demo-Project/src/main/java/com/example/demo_Project/Service/VisaApplicationService.java
@@ -1,4 +1,18 @@
 package com.example.demo_Project.Service;
 
+import com.example.demo_Project.Repositories.ApplicantRepository;
+import com.example.demo_Project.Repositories.VisaApplicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class VisaApplicationService {
+
+    @Autowired
+    VisaApplicationRepository visaApplicationRepository;
+
+    @Autowired
+    ApplicantRepository applicantRepository;
+
+
 }
