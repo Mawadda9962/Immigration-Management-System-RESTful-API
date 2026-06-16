c0d0d66b177c45c2ce46c0c629b1acbadb395bd8
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-16 14:21:53 +0400

    Implement RestAPI endpoints

--- demo-Project/src/main/java/com/example/demo_Project/Controllers/ApplicantController.java
+++ demo-Project/src/main/java/com/example/demo_Project/Controllers/ApplicantController.java
@@ -4,6 +4,8 @@ import com.example.demo_Project.DTO.ApplicantDTO;
 import com.example.demo_Project.Entities.Applicant;
 import com.example.demo_Project.Service.ApplicantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +19,13 @@ public class ApplicantController {
 
    // @Autowired
 
-    public ApplicantDTO registerApplicant(@RequestBody Applicant applicant){
-        return ApplicantDTO.convertToDTO(applicantService.saveApplicant(applicant));,
+    @PostMapping
+    public ResponseEntity<ApplicantDTO> registerApplicant(@RequestBody Applicant applicant){
+        return ResponseEntity.ok(ApplicantDTO.convertToDTO(applicantService.saveApplicant(applicant)));
     }
+
+    @PostMapping("/asylum")
+
 
 
 
--- demo-Project/src/main/java/com/example/demo_Project/Controllers/CenterController.java
+++ demo-Project/src/main/java/com/example/demo_Project/Controllers/CenterController.java
@@ -4,6 +4,7 @@ import com.example.demo_Project.DTO.CenterDTO;
 import com.example.demo_Project.Entities.ImmigrationCenter;
 import com.example.demo_Project.Service.CenterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,12 +16,12 @@ public class CenterController {
 
 
     @PostMapping
-    public CenterDTO addCenter(@RequestBody ImmigrationCenter center){
-        return CenterDTO.convertToDTO(centerService.createCenter(center));
+    public ResponseEntity<CenterDTO> addCenter(@RequestBody ImmigrationCenter center){
+        return ResponseEntity.ok(CenterDTO.convertToDTO(centerService.createCenter(center)));
     }
 
     @GetMapping("/{id}")
-    public CenterDTO getCenterById(@PathVariable Long id){
-        return CenterDTO.convertToDTO(centerService.getCenterByID(id));
+    public ResponseEntity<CenterDTO> getCenterById(@PathVariable Long id){
+        return ResponseEntity.ok(CenterDTO.convertToDTO(centerService.getCenterByID(id)));
     }
 }
