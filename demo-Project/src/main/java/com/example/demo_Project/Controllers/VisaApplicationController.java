ef0e512e917002599e85e3133e826c92a2a9b795
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-17 09:15:27 +0400

    Add VisaApplicationController

--- demo-Project/src/main/java/com/example/demo_Project/Controllers/VisaApplicationController.java
+++ demo-Project/src/main/java/com/example/demo_Project/Controllers/VisaApplicationController.java
@@ -1,11 +1,22 @@
 package com.example.demo_Project.Controllers;
 
+import com.example.demo_Project.Service.VisaApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("visas")
 public class VisaApplicationController {
+
+    @Autowired
+    VisaApplicationService visaApplicationService;
+
+    @PostMapping("/submit/{applicantId}")
+    public ResponseEntity
+
 
 
 }
