67e9bec82ad020b9b8efccfaf92c97dc2fbb7b2b
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-17 09:12:21 +0400

    Adding git visa by Status method

--- demo-Project/src/main/java/com/example/demo_Project/Controllers/VisaApplicationController.java
+++ demo-Project/src/main/java/com/example/demo_Project/Controllers/VisaApplicationController.java
@@ -1,6 +1,10 @@
 package com.example.demo_Project.Controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("visas")
 public class VisaApplicationController {
 
 
