17a961c9d0ba4ee1d2c249969c818a130f11fe66
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-17 09:16:47 +0400

    Add VisaApplicationController

--- demo-Project/src/main/java/com/example/demo_Project/Controllers/VisaApplicationController.java
+++ demo-Project/src/main/java/com/example/demo_Project/Controllers/VisaApplicationController.java
@@ -4,10 +4,7 @@ import com.example.demo_Project.DTO.VisaApplicationDTO;
 import com.example.demo_Project.Service.VisaApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("visas")
@@ -17,7 +14,7 @@ public class VisaApplicationController {
     VisaApplicationService visaApplicationService;
 
     @PostMapping("/submit/{applicantId}")
-    public ResponseEntity<VisaApplicationDTO> submitApplication(@PathVariable Long applicantId)
+    public ResponseEntity<VisaApplicationDTO> submitApplication(@PathVariable Long applicantId, @RequestParam String)
 
 
 
